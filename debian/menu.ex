531f3aacee19830ff78a2e7172a9b1e694cebfcd
Wesley Araujo <wesley.sickert@gmail.com>
2016-07-14 22:32:05 +0000

    removed manually unnecessary files

--- debian/menu.ex
+++
@@ -1,2 +0,0 @@
-?package(pyramid-chameleon):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
-  title="pyramid-chameleon" command="/usr/bin/pyramid-chameleon"
--- debian/pyramid-chameleon.cron.d.ex
+++
@@ -1,4 +0,0 @@
-#
-# Regular cron jobs for the pyramid-chameleon package
-#
-0 4	* * *	root	[ -x /usr/bin/pyramid-chameleon_maintenance ] && /usr/bin/pyramid-chameleon_maintenance
